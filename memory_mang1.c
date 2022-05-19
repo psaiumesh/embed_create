2632bd9db637fc8915e7250a424a8f23af98d5d3
bhagavan <bhagavan@auranetworks.in>
2022-05-19 16:12:58 +0530

    Optimizing the code

--- memory_mang1.c
+++ memory_mang1.c
@@ -3,64 +3,102 @@ int f1=0;
 int f2=0;
 char  b1[100];
 char  b2[100];
-char *my_malloc(int size)
+
+void * my_malloc(int size)
 {
 	if((f1==0) && (size <=sizeof(b1)))
 	{
 		f1=1;
-		return(b1);
+		return((void *)b1);
 	}
+
 	if(f2==0 && (size <=sizeof(b2)))
 	{
 		f2=1;
-		return(b2);
-	}
-	else if(size > sizeof(b1))
-	{
-		printf("requested memory than avaible  \n");
-		
-	}
-	else
-	{
-		printf("memory is not there \n");
+		return((void *)b2);
 	}
 
+	if(size > sizeof(b1))
+	{
+		printf("requested memory more than avaible\n");
+		return NULL;
+	}
+
+	printf("memory is not there \n");
+	return NULL;
+
 }
-char my_free(char **ptr)
+
+void my_free(void *ptr)
 {
 	if(f1==1)
 	{
 		f1=0;
-		*ptr=NULL;
 	}
 	else if(f2==1)
 	{
 		f2=0;
-		*ptr=NULL;
 	}
-
 }
 
 
+void who_is_free()
+{
+	if(!f1)
+		printf("B1 is FREE\n");
+	else
+		printf("B1 is ALLOCED\n");
+
+	if(!f2)
+		printf("B2 is FREE\n");
+	else
+		printf("B2 is ALLOCED\n");
+	printf("\n");
+}
+
+void test1()
+{
+	char *p1, *p2, *p3, *p4, *p5;;
+	p1 = p2 = p3 = p4 = NULL;
+
+	p1=my_malloc(100);
+	p2=my_malloc(100);
+	my_free(p1);
+	p1 = NULL;
+
+	my_free(p2);
+	p2 = NULL;
+
+	p1=my_malloc(80);
+	my_free(p1);
+	p1 = NULL;
+
+	p2=my_malloc(100);
+	my_free(p2);
+	p2 = NULL;
+
+	p3=my_malloc(100);
+}
+
+void test2()
+{
+	char *p1, *p2, *p3, *p4, *p5;;
+	p1 = p2 = p3 = p4 = NULL;
+	who_is_free();
+	p1=my_malloc(100);
+	who_is_free();
+	p2=my_malloc(100);
+	who_is_free();
+	my_free(p2);
+	who_is_free();
+	return;
+
+	p1=my_malloc(100);
+	p2=my_malloc(100);
+	my_free(p1);
+}
 
 int main()
 {
-	char  *p1=my_malloc(100);
-	char  *p2=my_malloc(100);
-	my_free(&p1);
-	my_free(&p2);
-	char *p3=my_malloc(80);
-	my_free(&p3);
-	char *p4=my_malloc(100);
-	my_free(&p4);
-	char *p5=my_malloc(100);
-	if(p1==b1 || p1==b2)
-		printf("p1 is allocated\n");
-	if(p2==b2 || p2==b1)
-		printf("p2 is allocated\n");
-	if(p3==b1 || p3==b2)
-		printf("p3 is allocated \n");
-	if(p5==b1 || p5==b2)
-		printf("p5 is allocated\n");
-	}
-
+	test2();
+}
